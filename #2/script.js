ca01e4e2cae62441e80940b8079aba4e417c9bf7
barnaLeo <156253946+barnaLeo@users.noreply.github.com>
2024-02-05 23:46:35 +0100

    Update script.js

--- #2/script.js
+++ #2/script.js
@@ -1,1 +1,31 @@
+var v1 = 50, v2 = 40, v3 = 30, v4 = 20, v5 = 10, v6 = 5;
 
+function equal1() {
+  var a = v1;
+  var b = v1;
+  return a + b;
+}
+
+function equal2() {
+  var a = v3;  // Set the appropriate value for a
+  var b = v4;  // Set the appropriate value for b
+  return a - b;  // Return value equal to 100
+}
+
+function equal3() {
+  var a = v6;  // Set the appropriate value for a
+  var b = v5;  // Set the appropriate value for b
+  return a * b;  // Return value equal to 100
+}
+
+function equal4() {
+  var a = v1;  // Set the appropriate value for a
+  var b = v2;  // Set the appropriate value for b
+  return a / b;  // Return value equal to 100
+}
+
+function equal5() {
+  var a = v1;  // Set the appropriate value for a
+  var b = v2;  // Set the appropriate value for b
+  return a % b;  // Return value equal to 100
+}
