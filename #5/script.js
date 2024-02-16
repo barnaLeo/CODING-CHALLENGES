dff1c6f4c937f5e16fe30795301b012a655aafa0
barnaLeo <156253946+barnaLeo@users.noreply.github.com>
2024-02-17 00:08:33 +0100

    Update script.js

--- #5/script.js
+++ #5/script.js
@@ -1,1 +1,5 @@
-
+function animal(obj) {
+  return "This " + obj.color + " " + obj.name + " has " + obj.legs + " legs.";
+}
+var myAnimal = {name: "dog", legs: 4, color: "white"};
+console.log(animal(myAnimal)); // Output: "This white dog has 4 legs."
