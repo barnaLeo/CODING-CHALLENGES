c30841dd4ab2f778eaad653a64d4917e14ba155d
barnaLeo <156253946+barnaLeo@users.noreply.github.com>
2024-03-02 23:51:28 +0100

    Update script.js

--- #7/script.js
+++ #7/script.js
@@ -1,1 +1,15 @@
+// Using if..else statement
+function saleHotdogs(n) {
+  if (n < 5) {
+    return n * 100;
+  } else if (n >= 5 && n < 10) {
+    return n * 95;
+  } else {
+    return n * 90;
+  }
+}
 
+// Using ternary operator
+function saleHotdogs(n) {
+  return n < 5 ? n * 100 : n >= 5 && n < 10 ? n * 95 : n * 90;
+}
