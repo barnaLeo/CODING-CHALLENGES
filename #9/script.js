bcbc511ad1f90c0e8297bfb3c3dcaa81a47d3b1c
barnaLeo <156253946+barnaLeo@users.noreply.github.com>
2024-03-03 23:56:19 +0100

    Update script.js

--- #9/script.js
+++ #9/script.js
@@ -1,1 +1,15 @@
+function padIt(str, n) {
+    let paddedStr = str;
+    let left = true;
 
+    for (let i = 0; i < n; i++) {
+        if (left) {
+            paddedStr = "*" + paddedStr;
+        } else {
+            paddedStr = paddedStr + "*";
+        }
+        left = !left;
+    }
+
+    return paddedStr;
+}
